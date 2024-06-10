9cfa070e2656ac9b03797c0db7a10c07789bb809
Roger Huauya Mamani <38589877+RogerHuauya@users.noreply.github.com>
2024-06-09 20:40:38 -0500

    OpenAPI added for Django (#13)

--- XDrive/urls.py
+++ XDrive/urls.py
@@ -15,6 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="XDrive API",
+        default_version='v1',
+        description="XDrive API for Uploading and Downloading Files",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="roger.huauya@utec.edu.pe"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,5 +38,8 @@ urlpatterns = [
     path('web/', include('web.urls')),  # Agrega esta línea
 
     path('upload/', include('upload.urls')),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 
 ]
