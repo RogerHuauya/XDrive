9de078da4c455b78a874512646d4cbf539a447c6
AlvaUtec <alvaro.garcia.h@utec.edu.pe>
2024-06-10 01:45:04 -0500

    List master files added

--- upload/views.py
+++ upload/views.py
@@ -1,6 +1,18 @@
 from .models import ChunkedFile, MasterFile
 from rest_framework import viewsets
 from .serializers import ChunkedFileSerializer, MasterFileSerializer
+from django.utils import timezone
+from django.views.generic import ListView
+
+
+class MasterFileListView(ListView):
+    model = MasterFile
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
 
 
 class MasterFileModelViewSet(viewsets.ModelViewSet):
