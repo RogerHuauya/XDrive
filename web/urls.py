4cdc75f339e5307059e64bc2afee7644acaae4b4
Alvamau <112715978+AlvaUtec@users.noreply.github.com>
2024-06-10 18:09:34 -0500

    List master files added (#17)

--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path("master_files/", views.masterFileListView, name="master_files"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from upload.models import MasterFile
 
 
 def home_page(request):
     return render(request, 'web/index.html')
+
+
+def masterFileListView(request):
+    master_files = MasterFile.objects.all()
+    return render(
+        request, "upload/masterfile_list.html", {"master_files": master_files}
+    )
