8c56bcf4c09ec9f1b302f784fb4af10288e80d08
jonyyy1 <jonathan.quilca@utec.edu.pe>
2024-06-27 01:26:02 -0500

    function version to upload y download files

--- upload/views.py
+++ upload/views.py
@@ -4,6 +4,15 @@ from .serializers import ChunkedFileSerializer, MasterFileSerializer
 from django.utils import timezone
 from django.views.generic import ListView
 
+from rest_framework import viewsets
+from .serializers import ChunkedFileSerializer, MasterFileSerializer
+from django.utils import timezone
+from django.views.generic import ListView
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.core.files.base import ContentFile
+from django.http import FileResponse,HttpResponse
 
 class MasterFileListView(ListView):
     model = MasterFile
@@ -23,3 +32,48 @@ class MasterFileModelViewSet(viewsets.ModelViewSet):
 class ChunkedFileModelViewSet(viewsets.ModelViewSet):
     queryset = ChunkedFile.objects.all()
     serializer_class = ChunkedFileSerializer
+
+
+class ChunkedFileModelViewSet(viewsets.ModelViewSet):
+    queryset = ChunkedFile.objects.all()
+    serializer_class = ChunkedFileSerializer
+
+    @action(detail=False, methods=['get'], url_path='last-chunk')
+    def last_chunk(self, request):
+        master_file_id = request.query_params.get('master_file_id')
+        if not master_file_id:
+            return Response({"error": "master_file_id parameter is required"}, status=400)
+
+        master_file = get_object_or_404(MasterFile, id=master_file_id)
+        last_chunk = ChunkedFile.objects.filter(master_file=master_file).order_by('-chunk_number').first()
+
+        if not last_chunk:
+            return Response({"message": "No chunks found for this master file"}, status=404)
+
+        serializer = self.get_serializer(last_chunk)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='download')
+    def download_file(self, request):
+        master_file_id = request.query_params.get('master_file_id')
+        if not master_file_id:
+            return Response({"error": "master_file_id parameter is required"}, status=400)
+
+        master_file = get_object_or_404(MasterFile, id=master_file_id)
+        chunks = ChunkedFile.objects.filter(master_file=master_file).order_by('chunk_number')
+
+        if not chunks.exists():
+            return Response({"error": "No chunks available for this master file"}, status=404)
+
+        # Combine the chunks into a single file
+        combined_file_content = b''
+        for chunk in chunks:
+            with chunk.file.open('rb') as f:
+                combined_file_content += f.read()
+
+        response = HttpResponse(combined_file_content, content_type='application/octet-stream')
+        response['Content-Disposition'] = f'attachment; filename="{master_file.file_name}"'
+        print(response['Content-Disposition'])
+        return response
+
+
--- web/static/scripts.js
+++ web/static/scripts.js
@@ -1,109 +1,311 @@
+function initIndexedDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open("uploadDatabase", 1);
 
-
-        function getCsrfToken() {
-            const csrfTokenElement = document.querySelector('[name=csrfmiddlewaretoken]');
-            if (!csrfTokenElement) {
-                throw new Error('CSRF token not found in the document');
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains("chunks")) {
+                const objectStore = db.createObjectStore("chunks", { keyPath: "id", autoIncrement: true });
+                objectStore.createIndex("fileId", "fileId", { unique: false });
+                objectStore.createIndex("chunkNumber", "chunkNumber", { unique: false });
             }
-            return csrfTokenElement.value;
-        }
 
-        function calculateMd5(blob) {
-            return new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onload = function(event) {
-                    const data = event.target.result;
-                    const md5 = CryptoJS.MD5(CryptoJS.enc.Latin1.parse(data)).toString();
-                    resolve(md5);
-                };
-                reader.onerror = function() {
-                    reject('File read error');
-                };
-                reader.readAsBinaryString(blob);
-            });
-        }
+        };
+
+        request.onsuccess = (event) => {
+            resolve(event.target.result);
+        };
+
+        request.onerror = (event) => {
+            reject(event.target.error);
+        };
+    });
+}
+
+async function storeChunkInIndexedDB(fileId, fileName, chunk, chunkNumber) {
+    const db = await initIndexedDB();
+    return new Promise((resolve, reject) => {
+        const transaction = db.transaction(["chunks"], "readwrite");
+        const objectStore = transaction.objectStore("chunks");
+        const request = objectStore.add({ fileId: fileId, chunkNumber: chunkNumber, fileName: fileName, chunk: chunk });
+
+        request.onsuccess = () => {
+            console.log(`Chunk ${chunkNumber} del archivo ${fileName} almacenado en IndexedDB`);
+            resolve();
+        };
+
+        request.onerror = (event) => {
+            reject(event.target.error);
+        };
+    });
+}
 
 
-        async function createMasterFile(fileName, totalChunks, md5Checksum, csrfToken) {
-            try {
-                const response = await axios.post('http://localhost:8000/upload/masterfile/', {
-                    file_name: fileName,
-                    md5_checksum: md5Checksum,
-                    number_of_chunks: totalChunks
-                }, {
-                    headers: {
-                        'X-CSRFToken': csrfToken
-                    }
-                });
+async function getChunksFromIndexedDB(fileId) {
+    const db = await initIndexedDB();
+    return new Promise((resolve, reject) => {
 
-                return response.data;
-            } catch (error) {
-                if (error.response) {
-                    throw new Error(`Failed to create master file: ${error.response.data}`);
-                } else {
-                    throw new Error('Failed to create master file');
-                }
+        const request = db
+              .transaction(["chunks"], "readonly")
+              .objectStore("chunks").index("fileId")
+              .getAll(Number(fileId));
+
+        request.onsuccess = (event) => {
+            console.log(`Chunks recuperados de IndexedDB para fileId ${fileId}`);
+            console.log(event.target.result)
+            resolve(event.target.result);
+        };
+
+        request.onerror = (event) => {
+            reject(event.target.error);
+        };
+    });
+}
+
+async function clearChunksFromIndexedDB(fileId) {
+    const db = await initIndexedDB();
+    return new Promise((resolve, reject) => {
+        const transaction = db.transaction(["chunks"], "readwrite");
+        const objectStore = transaction.objectStore("chunks");
+        const index = objectStore.index("fileId");
+        const request = index.openCursor(IDBKeyRange.only(fileId));
+
+        request.onsuccess = (event) => {
+            const cursor = event.target.result;
+            if (cursor) {
+                cursor.delete();
+                cursor.continue();
+            } else {
+                resolve();
             }
-        }
+        };
 
-        async function uploadChunk(fileId, chunk, chunkNumber, chunkMd5, csrfToken) {
-            const formData = new FormData();
-            formData.append('file', chunk);
-            formData.append('chunk_number', chunkNumber);
-            formData.append('master_file', fileId);
-            formData.append('md5_checksum', chunkMd5);
+        request.onerror = (event) => {
+            reject(event.target.error);
+        };
+    });
+}
 
-            try {
-                const response = await axios.post('http://localhost:8000/upload/chunkedfile/', formData, {
-                    headers: {
-                        'X-CSRFToken': csrfToken
-                    }
-                });
+function getCsrfToken() {
+    const csrfTokenElement = document.querySelector('[name=csrfmiddlewaretoken]');
+    if (!csrfTokenElement) {
+        throw new Error('CSRF token not found in the document');
+    }
+    return csrfTokenElement.value;
+}
 
-                return response.data;
-            } catch (error) {
-                if (error.response) {
-                    throw new Error(`HTTP error! status: ${error.response.status}: ${error.response.data}`);
-                } else {
-                    throw new Error('HTTP error! status: Network Error');
-                }
+function calculateMd5(blob) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const data = event.target.result;
+            const md5 = CryptoJS.MD5(CryptoJS.enc.Latin1.parse(data)).toString();
+            resolve(md5);
+        };
+        reader.onerror = function() {
+            reject('File read error');
+        };
+        reader.readAsBinaryString(blob);
+    });
+}
+
+async function createMasterFile(fileName, totalChunks, md5Checksum, csrfToken) {
+    try {
+        const response = await axios.post('/upload/masterfile/', {
+            file_name: fileName,
+            md5_checksum: md5Checksum,
+            number_of_chunks: totalChunks
+        }, {
+            headers: {
+                'X-CSRFToken': csrfToken
             }
-        }
-
-        // Manejar la subida completa del archivo
-        async function uploadFile(event) {
-            console.log("1 - Iniciando función uploadFile");
-            event.preventDefault();
-
-            try {
-                const fileInput = document.getElementById('file');
-                const file = fileInput.files[0];
-                const chunkSize = 1024 * 1024; // 1MB
-                const totalChunks = Math.ceil(file.size / chunkSize);
-                const csrfToken = getCsrfToken();
-
-                // Calcular MD5 del archivo completo
-                const fileMd5 = await calculateMd5(file);
-
-                const masterFile = await createMasterFile(file.name, totalChunks, fileMd5, csrfToken);
-                const fileId = masterFile.id;
-                console.log("1 - MasterFile creado con ID:", fileId);
-
-                for (let i = 0; i < totalChunks; i++) {
-                    const chunk = file.slice(i * chunkSize, (i + 1) * chunkSize);
-                    const chunkMd5 = await calculateMd5(chunk);
-                    console.log(`2 - Subiendo chunk ${i + 1} de ${totalChunks} con MD5: ${chunkMd5}`);
-                    await uploadChunk(fileId, chunk, i, chunkMd5, csrfToken);
-                }
-
-                console.log('3 - Archivo subido exitosamente!');
-                alert('File uploaded successfully!');
-            } catch (error) {
-                console.error('Error during file upload:', error);
-            }
-        }
-
-        document.addEventListener('DOMContentLoaded', (event) => {
-            const form = document.getElementById('uploadForm');
-            form.addEventListener('submit', uploadFile);
         });
+        return response.data;
+    } catch (error) {
+        handleError(error, 'Failed to create master file');
+    }
+}
+
+let simulateNetworkFailureAfterChunks = 2; // Simular falla después de 3 chunks subidos
+
+async function uploadChunkToServer(masterFileId, chunk, chunkNumber, chunkMd5, csrfToken) {
+    const formData = new FormData();
+    formData.append('file', chunk);
+    formData.append('chunk_number', chunkNumber);
+    formData.append('master_file', masterFileId);
+    formData.append('md5_checksum', chunkMd5);
+
+    try {
+        const response = await axios.post('/upload/chunkedfile/', formData, {
+            headers: {
+                'X-CSRFToken': csrfToken
+            }
+        });
+        return response.data;
+    } catch (error) {
+        handleError(error, 'Failed to components chunk');
+    }
+}
+
+async function getLastUploadedChunk(masterFileId) {
+    try {
+        const response = await axios.get(`/upload/chunkedfile/last-chunk/?master_file_id=${masterFileId}`);
+        console.log(`Último chunk subido: ${response.data.chunk_number}`); // Depuración
+        return response.data.chunk_number;
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            console.warn(`Master file with ID ${masterFileId} not found.`);
+            return 0;
+        } else {
+            handleError(error, 'Failed to fetch last uploaded chunk');
+        }
+    }
+}
+
+function handleError(error, message) {
+    if (error.response) {
+        console.error(`${message}: ${error.response.data}`);
+    } else {
+        console.error(`${message}: Network Error`);
+    }
+    throw new Error(message);
+}
+
+async function updateMasterFileStatus(masterFileId, status, csrfToken) {
+    try {
+        const response = await axios.patch(`/upload/masterfile/${masterFileId}/`, {
+            status: status
+        }, {
+            headers: {
+                'X-CSRFToken': csrfToken
+            }
+        });
+        return response.data;
+    } catch (error) {
+        handleError(error, 'Failed to update master file status');
+    }
+}
+
+async function uploadFile(event, masterFileId = null, startChunk = 0) {
+    console.log("1 - Iniciando función uploadFile");
+    event.preventDefault();
+
+    try {
+        const fileInput = document.getElementById(masterFileId ? `file-input-${masterFileId}` : 'file');
+        const file = fileInput.files[0];
+        const totalChunks = 5; // Dividir en 5 partes
+        const chunkSize = Math.ceil(file.size / totalChunks); // Tamaño de cada chunk
+        const csrfToken = getCsrfToken();
+        const fileMd5 = await calculateMd5(file);
+
+        if (!masterFileId) {
+            const masterFile = await createMasterFile(file.name, totalChunks, fileMd5, csrfToken);
+            masterFileId = masterFile.id;
+        } else {
+            const lastUploadedChunk = await getLastUploadedChunk(masterFileId);
+            startChunk = lastUploadedChunk ? lastUploadedChunk + 1 : 0;
+        }
+
+        for (let i = startChunk; i < totalChunks; i++) {
+            const chunk = file.slice(i * chunkSize, (i + 1) * chunkSize);
+            const chunkMd5 = await calculateMd5(chunk);
+            console.log(`2 - Subiendo chunk ${i + 1} de ${totalChunks} con MD5: ${chunkMd5}`);
+            try {
+                await uploadChunkToServer(masterFileId, chunk, i, chunkMd5, csrfToken);
+            } catch (error) {
+                console.warn(`Error subiendo chunk ${i + 1}, almacenando en IndexedDB para reanudar luego`);
+                // Almacenar todos los chunks faltantes en IndexedDB
+                for (let j = i; j < totalChunks; j++) {
+                    const remainingChunk = file.slice(j * chunkSize, (j + 1) * chunkSize);
+                    await storeChunkInIndexedDB(masterFileId, file.name, remainingChunk, j);
+                }
+                throw error;
+            }
+            if (i === startChunk) {
+                await updateMasterFileStatus(masterFileId, 'in_progress', csrfToken);
+            }
+        }
+
+        await updateMasterFileStatus(masterFileId, 'completed', csrfToken);
+        await clearChunksFromIndexedDB(masterFileId); //
+
+        console.log('4 - Archivo subido exitosamente!');
+    } catch (error) {
+        console.error('Error durante la subida del archivo:', error);
+    }
+}
+
+
+async function printIndexedDBContents() {
+    const db = await initIndexedDB();
+    return new Promise((resolve, reject) => {
+        const transaction = db.transaction(["chunks"], "readonly");
+        const objectStore = transaction.objectStore("chunks");
+        const request = objectStore.getAll();
+
+        request.onsuccess = (event) => {
+            console.log('IndexedDB Contents:', event.target.result);
+            resolve(event.target.result);
+        };
+
+        request.onerror = (event) => {
+            reject(event.target.error);
+        };
+    });
+}
+
+    async function resumeUpload(event, masterFileId) {
+        event.preventDefault();
+        const csrfToken = getCsrfTokened();
+
+        const storedChunks = await getChunksFromIndexedDB(masterFileId);
+        if (storedChunks.length > 0) {
+            console.log(`Reanudando la carga desde IndexedDB, chunk ${storedChunks[0].chunkNumber + 1}`);
+            for (const {fileId, chunkNumber, chunk } of storedChunks) {
+                const chunkMd5 = await calculateMd5(chunk);
+                console.log(`Subiendo chunk ${chunkNumber + 1} desde IndexedDB`);
+                console.log("MasterFileId : "+fileId)
+                console.log("File : "+chunk)
+                console.log("ChunkNumber : "+chunkNumber)
+                console.log("ChunkMd5 : "+chunkMd5)
+                console.log("CsrfToken : "+csrfToken)
+                await uploadChunkToServer(fileId, chunk, chunkNumber, chunkMd5, csrfToken);
+
+            }
+            await updateMasterFileStatus(masterFileId, 'completed', csrfToken);
+            await clearChunksFromIndexedDB(masterFileId);
+        }
+    }
+
+    async function downloadFile(masterFileId) {
+        try {
+            const response = await axios.get(`/upload/chunkedfile/download/?master_file_id=${masterFileId}`, {
+                responseType: 'blob'
+            });
+
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+
+            const contentDisposition = response.headers['content-disposition'];
+            console.log("Content-Disposition:", contentDisposition); // Depuración
+
+            let fileName = 'downloaded_file.png';
+            if (contentDisposition) {
+                const matches = /filename="([^"]*)"/.exec(contentDisposition);
+                if (matches != null && matches[1]) {
+                    fileName = matches[1];
+                }
+            }
+            link.setAttribute('download', fileName);
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } catch (error) {
+            console.error('Error downloading file:', error);
+        }
+    }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    printIndexedDBContents();
+});
